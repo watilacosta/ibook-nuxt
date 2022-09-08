597ae43406cc261892722ab6a3c5ea8f8af88821
Watila Costa <watila.costa@redealumni.com>
2022-09-07 23:11:03 -0300

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,48 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'iBooks',
+    htmlAttrs: {
+      lang: 'pt-BR',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description',
+        name: 'description',
+        content: 'Leia mais conosco!'
+      },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ['normalize.css/normalize.css', '@/assets/scss/base.scss'],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: [{ path: '@/components', pathPrefix: false }],
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: ['@nuxtjs/axios', '@nuxtjs/style-resources'],
+
+  styleResources: {
+    scss: ['@/components/bosons/*.scss'],
+  },
+
+  axios: {
+    baseURL: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000'
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <h1>Home</h1>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    layout: 'ibook'
+  })
+</script>
